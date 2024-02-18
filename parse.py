407c19f653c7334bc409758e7556e26eab8cfe16
Gaspard Petit <gaspardpetit@gmail.com>
2024-02-18 09:34:53 -0500

    Add basic support for html to markdown

--- parse.py
+++ parse.py
@@ -12,3 +12,28 @@ for url in src:
         formatter = fed.FormatterMarkdown(f)
         formatter.format(act)
 
+for url in ["https://s3.ca-central-1.amazonaws.com/manuels-manuals-opic-cipo/MOPOP_English.html"]:
+    import requests
+    import html2text
+    import os
+    def html_to_markdown(url):
+        response = requests.get(url)
+        
+        # Check if the request was successful
+        if response.status_code == 200:
+            # Convert HTML to Markdown
+            converter = html2text.HTML2Text()
+            converter.body_width = 0  # Set to 0 for no line wrapping
+            markdown_content = converter.handle(response.text)
+            
+            return markdown_content
+        else:
+            print(f"Failed to fetch content from {url}. Status code: {response.status_code}")
+            return None
+    
+    md = html_to_markdown(url)
+    file_name = url.split("/")[-1]
+    file_name, _ = os.path.splitext(file_name)
+    file_name = f"out/{file_name}.md"
+    with open(file_name, "w", encoding="utf-8") as f:
+        f.write(md)
